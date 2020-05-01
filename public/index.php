b22a8a787822656719b623d388b70263253c8d5e
TheKito <11139894+TheKito@users.noreply.github.com>
2020-04-30 21:10:25 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
 
-header('Location: //' . substr($_SERVER['HTTP_HOST'], strpos($_SERVER['HTTP_HOST'] + 1, '.')) . $_SERVER['REQUEST_URI']);
+header('Location: //' . substr($_SERVER['HTTP_HOST'], strpos($_SERVER['HTTP_HOST'], '.') + 1) . $_SERVER['REQUEST_URI']);
 
